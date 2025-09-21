55724094ec0325322efa9a2fb24e7d3ea4c2822e
Brody Michaels <brodygmichaels@gmail.com>
2025-09-20 23:18:43 -0400

    Fixed image reference

--- src/data.js
+++ src/data.js
@@ -1,19 +1,19 @@
 export const entries = [
     {
         date: "2025-09-20",
-        images: ["./public/banana.jpg", "./public/elephant.jpg"],
+        images: ["banana.jpg", "elephant.jpg"],
         desc: "Random",
         status: "completed"
     },
     {
         date: "2025-09-19",
-        images: ["./public/tree.jpg"],
+        images: ["tree.jpg"],
         desc: "Okayy",
         status: "in progress"
     },
     {
         date: "2025-09-18",
-        images: ["./public/react.svg"],
+        images: ["react.svg"],
         desc: "yes",
         status: "completed"
     }
